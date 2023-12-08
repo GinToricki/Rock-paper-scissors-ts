216a83d4a9e7edbf7db37315f53d7e23a4da9ab1
Tin Goricki <tingoricki1@protonmail.com>
2023-12-08 14:08:55 +0100

    Added a function to play a round

--- Rock-paper-scissors-ts/src/main.ts
+++ Rock-paper-scissors-ts/src/main.ts
@@ -1,7 +1,7 @@
 function getComputerChoice() {
   let choice = Math.floor(Math.random()*3)
 
-  let choiceString;
+  let choiceString = "";
 
   switch(choice){
     case 0:
@@ -18,4 +18,20 @@ function getComputerChoice() {
       break;
   }
   return choiceString
+}
+
+function playRound(computerChoice: string, playerChoice: string) {
+  playerChoice.toLowerCase();
+
+  if( playerChoice == computerChoice ) {
+    console.log("Tie")
+  }else if(playerChoice == "scissors" && computerChoice == "paper") {
+    console.log("You win! Scissors beat paper")
+  }else if(playerChoice == "rock" && computerChoice == "scissors") {
+    console.log("You win! Rock beats scissors")
+  }else if(playerChoice == "paper" && computerChoice == "rock"){
+    console.log("You win! Paper beats rock")
+  }else {
+    console.log("You lose")
+  }
 }
