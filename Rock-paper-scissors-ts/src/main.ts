733ae5b49c879a9dfafc58b80a5ccd235cf35949
Tin Goricki <tingoricki1@protonmail.com>
2023-12-08 14:27:29 +0100

    Added game function

--- Rock-paper-scissors-ts/src/main.ts
+++ Rock-paper-scissors-ts/src/main.ts
@@ -20,18 +20,56 @@ function getComputerChoice() {
   return choiceString
 }
 
-function playRound(computerChoice: string, playerChoice: string) {
-  playerChoice.toLowerCase();
+function playRound(computerChoice: string, playerChoice: string | null) {
+  if(playerChoice==null) {
+    return 0
+  }else {
+    playerChoice.toLowerCase();
+  }
 
   if( playerChoice == computerChoice ) {
-    console.log("Tie")
+    console.log("tie");
+    return 0
   }else if(playerChoice == "scissors" && computerChoice == "paper") {
-    console.log("You win! Scissors beat paper")
+    console.log("You win. " + playerChoice + " beats " + computerChoice + "!");
+    return 1
   }else if(playerChoice == "rock" && computerChoice == "scissors") {
-    console.log("You win! Rock beats scissors")
+    console.log("You win. " + playerChoice + " beats " + computerChoice + "!");
+    return 1
   }else if(playerChoice == "paper" && computerChoice == "rock"){
-    console.log("You win! Paper beats rock")
+    console.log("You win. " + playerChoice + " beats " + computerChoice + "!");
+    return 1
   }else {
-    console.log("You lose")
+    console.log("You lose. " + computerChoice + " beats " + playerChoice + "!")
+    return 2
   }
 }
+
+function game() {
+  let playerScore = 0;
+  let computerScore = 0;
+
+  for(let i = 0; i < 5; i++ ) {
+    let playerChoice = prompt()
+    switch(playRound(getComputerChoice(), playerChoice)) {
+      case 1:
+        playerScore++;
+        break;
+      case 2:
+        computerScore++;
+        break;
+      default:
+        computerScore++;
+        playerScore++;
+        break;
+    }
+  }
+
+  if(playerScore>computerScore) {
+    console.log("Player wins!");
+  }else{
+    console.log("Computer wins!")
+  }
+}
+
+game();
